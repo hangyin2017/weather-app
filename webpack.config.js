d272d27454de4b68ec98e12ac05a530721dac8c9
Hang <hang.yin2017@outlook.com>
2020-09-22 10:54:32 +1000

    style: Ajusted font sizes. Centralised container.

--- webpack.config.js
+++ webpack.config.js
@@ -26,10 +26,10 @@ module.exports = {
           },
         },
       },
-      {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
-      },
+      // {
+      //   test: /\.css$/,
+      //   use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      // },
       {
         test: /\.s[ac]ss$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
