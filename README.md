02b7e12df19ee63bf040e5b32fbfce18fd821f36
Hang <hang.yin2017@outlook.com>
2020-10-02 12:12:34 +1000

    build: Introduced .env in OpenWeatherApp. Created README.md.

---
+++ README.md
@@ -0,0 +1,10 @@
+### How to Start
+1. Create .env file in the root directory.
+
+2. In .env file, put in your APPID for OpenWeatherMap's API, in the format of:
+
+    APPID = *your app id*
+
+3. `npm install`
+
+4. `npm start`
--- src/apis/OpenWeatherMap/OpenWeatherMap.js
+++ src/apis/OpenWeatherMap/OpenWeatherMap.js
@@ -1,10 +1,8 @@
 const OpenWeatherMap = (type, id, units = 'metric') => {
   const BASE_URL = 'https://api.openweathermap.org/data/2.5';
-  const appid = '216d2848c9468ebc32e6b1975c3ebc54';
-  // const APPID = process.env.REACT_APP_APPID;
-  console.log(process.env.REACT_APP_APPID);
+  const APPID = process.env.APPID;
   
-  const query = `/${type}?id=${id}&units=${units}&appid=${appid}`;
+  const query = `/${type}?id=${id}&units=${units}&appid=${APPID}`;
 
   return fetch(BASE_URL + query).then(res => res.json());
 };
